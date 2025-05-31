50ee7dbea46c440d36dd80116b4865892a4ca842
Crush2161 <s2595229@gmail.com>
2025-06-01 02:04:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Crush2161/features",
+    "https://github.com/Crush2161/featuresss",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
